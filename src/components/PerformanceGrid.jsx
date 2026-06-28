c8c0864f138462f3074044eb03664d17e4558176
jppancho-va <imyours10202022@gmail.com>
2026-06-27 22:50:59 -0400

    Add PerformanceGrid component to App

---
+++ src/components/PerformanceGrid.jsx
@@ -0,0 +1,13 @@
+import PerformanceGrid from './PerformanceGrid'; // Adjust path if needed
+
+function App() {
+  return (
+    <div>
+      {/* ... other parts of your site ... */}
+      
+      <PerformanceGrid /> 
+      
+      {/* ... other parts of your site ... */}
+    </div>
+  );
+}

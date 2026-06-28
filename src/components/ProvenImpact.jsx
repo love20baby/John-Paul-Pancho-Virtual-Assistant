5f25bfc7a7cdaadf5a6c43dc04bffb72bf3d06bc
jppancho-va <imyours10202022@gmail.com>
2026-06-28 17:31:34 -0400

    Update ProvenImpact.jsx

--- src/components/ProvenImpact.jsx
+++ src/components/ProvenImpact.jsx
@@ -1,1 +1,11 @@
-zip 
+// src/components/ProvenImpact.jsx
+export default function ProvenImpact() {
+  return (
+    <section className="proven-impact">
+      <div className="wrap">
+        <h2>Proven Impact</h2>
+        {/* Content goes here */}
+      </div>
+    </section>
+  );
+}
